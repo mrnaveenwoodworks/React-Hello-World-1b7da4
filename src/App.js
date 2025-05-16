2434cb66edcb7c46f4905c6a03fd999e8412f214
mrnaveenwoodworks <mrnaveenwoodworks@gmail.com>
2025-05-16 18:45:17 +0530

    CODEMATE.ai - upload codebase

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ fontSize: "72px", textAlign: "center", marginTop: "40vh" }}>
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
